b4650e453c803701a842ce9164e0c804bc07d9b9
ShanFravinCSC <shanfravincsc@gmail.com>
2021-07-22 22:52:48 +0530

    poster carousel config

--- src/config/PosterCarousel.config.js
+++ src/config/PosterCarousel.config.js
@@ -2,29 +2,29 @@ const settings = {
     infinite: false,
     autoplay: false,
     slidesToShow: 5,
-    slidesToScroll: 2,
+    slidesToScroll: 4,
     InitialSlide: 0,
     responsive: [
         {
-        breakpoints: 1024,
+        breakpoint: 1024,
         settings: {
             slidesToShow:3,
-            slidesToScroll:2,
+            slidesToScroll:3,
             infinite: true,
         },
     },
     {
-        breakpoints: 600,
+        breakpoint: 600,
         settings: {
             slidesToShow:2,
-            slidesToScroll:1,
-            InitialSlide: 1,
+            slidesToScroll:2,
+            InitialSlide: 2,
         },
     },
     {
-        breakpoints: 480,
+        breakpoint: 480,
         settings: {
-            slidesToShow:2,
+            slidesToShow:3,
             slidesToScroll:1,
         },
     },
