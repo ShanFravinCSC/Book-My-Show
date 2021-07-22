9f2a4fbdcbdcbdfe80d2a63ea6cdc615a2117d01
ShanFravinCSC <shanfravincsc@gmail.com>
2021-07-22 19:51:28 +0530

    Home Page completed

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,15 +2,64 @@ import React from "react";
 
 //components
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
-import Premiere from "../components/Premiere/Premiere.component";
+import PosterSlider from "../components/PosterSlider/PosterSlider.component";
+
+//config
+import TempPosters from "../config/TempPosters.config";
+
 
 const HomePage = () =>{
     return(
         <>
+        <div className="flex flex-col gap-10">
             <div className="container mx-auto px-4">
                 <h1 className="text-xl font-bold text-gray-800 my-3">The best of Entertainment</h1>
                 <EntertainmentCardSlider />
-                <Premiere />
+                </div>
+            
+                <div className="bg-bms-800 py-12 ">
+                        
+                    <div className="container mx-auto px-4 flex flex-col gap-3">
+                    <div className="hidden md:flex">
+                            <img 
+                                src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+                                alt="Rupay"
+                                className="w-full h-full"
+                                />
+                        </div>
+                        <PosterSlider 
+                            images={TempPosters} 
+                            title="Premieres" 
+                            subtitle="Brand new releases every Friday"
+                            isDark
+                        />
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="container mx-auto px-4 my-8">
+            <PosterSlider 
+                images={TempPosters} 
+                title="OnLine Streaming Events"
+                isDark={false}
+            />
+            </div>
+
+            <div className="container mx-auto px-4 my-8">
+            <PosterSlider 
+                images={TempPosters} 
+                title="Outdoor Events"
+                isDark={false}
+            />
+            </div>
+
+            <div className="container mx-auto px-4 my-8">
+            <PosterSlider 
+                images={TempPosters} 
+                title="Laughter Therapy"
+                isDark={false}
+            />
             </div>
         </>
     );
