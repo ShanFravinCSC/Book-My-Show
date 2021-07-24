6ec73a58b21cf8715e15a0bc19c54f805184da9c
ShanFravinCSC <shanfravincsc@gmail.com>
2021-07-24 23:13:49 +0530

    Add Tmdb API

--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSlider from "react-slick";
+import axios from "axios";
 
 //Import arrows function
 import { NextArrow, PrevArrow } from "./Arrows.component";
 
 const HeroCarousel = () =>{
 
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+
+        const requestNowPlayingMovies = async () => {
+            const getImages = await axios.get("/movie/now_playing");
+            setImages(getImages.data.results);
+        };
+
+        requestNowPlayingMovies();
+    }, []);
+
 //for pc screen
-const settingsLG = {
+    const settingsLG = {
     arrows: true,
     autoplay: true,
     centerMode: true,
@@ -17,9 +30,9 @@ const settingsLG = {
     slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
-};
+    };
 //for tab and mobile screen
-const settings = {
+    const settings = {
     arrows:true,
     infinite: true,
     speed: 500,
@@ -27,15 +40,8 @@ const settings = {
     slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
-  };
+    };
 
-  const images = [
-      "https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-      "https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0Mnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-      "https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-      "https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-      "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-  ];
     return(
         <>
             <div className="lg:hidden">
@@ -43,7 +49,10 @@ const settings = {
                 {
                     images.map((image) => (
                         <div className="w-full h-56 md:h-80 py-3">
-                            <img src={image} alt="testing" className="w-full h-full" />
+                            <img 
+                                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`} 
+                                alt="testing" 
+                                className="w-full h-full" />
                         </div>
                     ))
                 }
@@ -55,7 +64,10 @@ const settings = {
                 {
                     images.map((image) => (
                         <div className="w-full h-96 px-2 py-3">
-                            <img src={image} alt="testing" className="w-full h-full rounded-md" />
+                            <img 
+                                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}  
+                                alt="testing"  
+                                className="w-full h-full rounded-md" />
                         </div>
                     ))
                 }
