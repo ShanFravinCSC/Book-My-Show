bccf7a568622fd85f2f11728fe9b6e76f23ec60c
ShanFravinCSC <shanfravincsc@gmail.com>
2021-07-25 22:54:50 +0530

    Add Razorpay integration

--- src/components/PaymentModal/Payment.component.jsx
+++ src/components/PaymentModal/Payment.component.jsx
@@ -7,6 +7,24 @@ export default function PaymentModal({ isOpen, setIsOpen, price }) {
     setIsOpen(false)
   }
 
+  const launchRazorPay = () =>{
+      let options = {
+          key: "rzp_test_jzxbYFG0qdpWbP",
+          amount: price * 100,
+          currency: "INR",
+          name: "Book My Show Clone",
+          description: "Movie Purchase or Rental",
+          image: "https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png",
+          handler: () => {
+              setIsOpen(false);
+              alert("Payment is Done");
+          },
+          theme:{ color: "#c4242d" },
+      };
+      const RazorPay = new window.Razorpay(options);
+      RazorPay.open();
+  };
+
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
@@ -61,7 +79,7 @@ export default function PaymentModal({ isOpen, setIsOpen, price }) {
                   <button
                     type="button"
                     className="w-full inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent hover:bg-red-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
-                    onClick={closeModal}
+                    onClick={launchRazorPay}
                   >
                     Pay ₹{price}
                   </button>
